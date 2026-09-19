1454eb67108abcf9d4dbcf056b45a6f7ce68f289
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-19 22:49:59 +0000

    main関数もEBOに対応させた

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 #include "mesh.hpp"
 #include "shader.hpp"
+#include "type.hpp"
 #include "window.hpp"
 
 // 1. バーテックスシェーダー（頂点の位置をそのまま通過させる）
@@ -35,21 +36,33 @@ auto main() -> int {
     if (not shader_result) return 1;
     auto shader = std::move(*shader_result);
 
-    constexpr auto vertices = std::array<float, 9>{
-        -0.5f,  // 左下 X
-        -0.5f,  //     Y
-        0.0f,   //     Z
-
-        0.5f,  // 右下
+    // 1. 四角形の4頂点 (X, Y, Z)
+    constexpr auto vertices = std::array<f32, 12>{
+        -0.5f,  // 0: 左下
         -0.5f,
         0.0f,
-
-        0.0f,  // 上
+        0.5f,  // 1: 右下
+        -0.5f,
+        0.0f,
+        0.5f,  // 2: 右上
+        0.5f,
+        0.0f,
+        -0.5f,  // 3: 左上
         0.5f,
         0.0f
     };
 
-    auto triangle = Mesh{vertices, 3};
+    // 2. 頂点をつなぐインデックス（時計回りまたは反時計回りで統一）
+    constexpr auto indices = std::array<u32, 6>{
+        0,  // 1つ目の三角形 (左下 -> 右下 -> 右上)
+        1,
+        2,
+        2,  // 2つ目の三角形 (右上 -> 左上 -> 左下)
+        3,
+        2
+    };
+
+    auto triangle = Mesh{vertices, indices, 3};
     while (window.is_open()) {
         window.clear();
 
