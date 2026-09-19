6e7acfc2986620b2c3a86a52a246c0c366610425
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-19 21:46:36 +0000

    drawメソッドを追加

--- include/mesh.hpp
+++ include/mesh.hpp
@@ -11,7 +11,8 @@
 namespace rin {
 class Mesh final {
   public:
-    explicit Mesh(const std::span<const f32> vertices, const u32 vertex_count) noexcept {
+    explicit Mesh(const std::span<const f32> vertices, const u32 vertex_count) noexcept
+        : vertex_count_{vertex_count} {
         glGenVertexArrays(1, &vao_);
         glGenBuffers(1, &vbo_);
 
@@ -45,15 +46,18 @@ class Mesh final {
     Mesh(const Mesh&) noexcept                    = delete;
     auto operator=(const Mesh&) noexcept -> Mesh& = delete;
     Mesh(Mesh&& other) noexcept
-        : vao_{std::exchange(other.vao_, 0)}, vbo_{std::exchange(other.vbo_, 0)} {}
+        : vao_{std::exchange(other.vao_, 0)},
+          vbo_{std::exchange(other.vbo_, 0)},
+          vertex_count_{std::exchange(other.vertex_count_, 0)} {}
     auto operator=(Mesh&& other) noexcept -> Mesh& {
         if (this == &other) return *this;
 
         if (vao_ != 0) glDeleteVertexArrays(1, &vao_);
         if (vbo_ != 0) glDeleteBuffers(1, &vbo_);
 
-        vao_ = std::exchange(other.vao_, 0);
-        vbo_ = std::exchange(other.vbo_, 0);
+        vao_          = std::exchange(other.vao_, 0);
+        vbo_          = std::exchange(other.vbo_, 0);
+        vertex_count_ = std::exchange(other.vertex_count_, 0);
 
         return *this;
     }
@@ -62,8 +66,15 @@ class Mesh final {
         if (vbo_ != 0) glDeleteBuffers(1, &vbo_);
     }
 
+    void draw() const noexcept {
+        glBindVertexArray(vao_);
+        glDrawArrays(GL_TRIANGLES, 0, static_cast<GLsizei>(vertex_count_));
+        glBindVertexArray(0);
+    }
+
   private:
     GLuint vao_{};
     GLuint vbo_{};
+    u32    vertex_count_{};
 };
 }  // namespace rin
