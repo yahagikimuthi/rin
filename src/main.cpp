ed819b99d79b2aefc6b95eeb51b78b5c142b3e68
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-20 14:00:16 +0000

    エラーを報告するコードを追加

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,18 @@ using namespace rin;
 
 auto main() -> int {
     auto windowResult = Window::create(800, 600, "Title");
-    if (not windowResult) return 1;
+    if (not windowResult) {
+        windowResult.error().what();
+        return 1;
+    }
     auto window = std::move(*windowResult);
 
-    auto quad = Quad{1.f, 5.f};
+    auto quadResult = Quad::create(1.f, 5.f);
+    if (not quadResult) {
+        quadResult.error().what();
+        return 1;
+    }
+    auto quad = std::move(*quadResult);
 
     while (window.is_open()) {
         window.begin_frame();
