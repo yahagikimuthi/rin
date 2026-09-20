3b0475ce290cc41d3d7931dd8ed0de2ea3bd0483
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-20 11:50:44 +0000

    Vector2, RGB, Errorクラス追加

--- include/util.hpp
+++ include/util.hpp
@@ -1,20 +1,52 @@
 #pragma once
 
-#include <fstream>
-#include <optional>
-#include <sstream>
-#include <string>
+#include <concepts>
+#include <iostream>
 #include <string_view>
 
+#include "type.hpp"
+
 namespace rin {
-[[nodiscard]] inline auto read_file(const std::string_view path) noexcept
-    -> std::optional<std::string> {
-    auto file = std::ifstream{std::string{path}, std::ios::in | std::ios::binary};
+template <typename T>
+concept Numeric = std::integral<T> or std::floating_point<T>;
 
-    if (not file.is_open()) return std::nullopt;
+template <Numeric T>
+struct Vector2 final {
+    T x;
+    T y;
+};
 
-    auto stream = std::stringstream{};
-    stream << file.rdbuf();
-    return stream.str();
-}
+using Vector2f = Vector2<f32>;
+
+struct RGB final {
+    f32 r;
+    f32 g;
+    f32 b;
+};
+
+class Error final {
+  public:
+    enum class Type : u8 { logic, runtime };
+
+    explicit Error(const Type type) noexcept : type_{type} {}
+    explicit Error(const Type type, const std::string_view message)
+        : type_{type}, message_{message} {}
+
+    [[nodiscard]] auto type() const noexcept -> Type { return type_; }
+
+    [[nodiscard]] auto message() const noexcept -> std::string_view { return message_; }
+
+    void what() const noexcept {
+        if (type_ == Type::logic) {
+            std::cerr << "[Logic Error]\n";
+        } else {
+            std::cerr << "[Runtime Error]\n";
+        }
+        std::cerr << message_ << '\n';
+    }
+
+  private:
+    const Type             type_;
+    const std::string_view message_;
+};
 }  // namespace rin
