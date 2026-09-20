f9bdceffd3ae991870c484359ce2bfdc9c8f2dd7
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-20 14:13:16 +0000

    ShaderのソースコードをUV座標に対応するよう変更

--- include/shader.hpp
+++ include/shader.hpp
@@ -10,27 +10,34 @@
 #include "util.hpp"
 
 namespace rin {
-// バーテックスシェーダー（頂点の位置をそのまま通過）
 constexpr const char* vertex_shader_source = R"(
     #version 450 core
+    
     layout (location = 0) in vec3 aPos;
     layout (location = 1) in vec3 aColor;
+    layout (location = 2) in vec2 aTexCoord;
 
     out vec3 ourColor;
+    out vec2 TexCoord;
 
     void main() {
         gl_Position = vec4(aPos, 1.0);
         ourColor = aColor;
+        TexCoord = aTexCoord;
     }
 )";
 
-// フラグメントシェーダー（ピクセルをオレンジ色に）
 constexpr const char* fragment_shader_source = R"(
     #version 450 core
     in vec3 ourColor;
+    in vec2 TexCoord;
+
     out vec4 FragColor;
+
+    uniform sampler2D ourTexture;
+
     void main() {
-        FragColor = vec4(ourColor, 1.0f);
+        FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
     }
 )";
 
