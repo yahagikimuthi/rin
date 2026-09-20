6a7d22a7e122a0283623444f0eddc6a4a87b6e1e
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-20 21:26:07 +0000

    ファイル名を変更 不要となったVectorクラスを削除

---
+++ include/util.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "type.hpp"
+
+namespace rin {
+struct RGB final {
+    f32 r;
+    f32 g;
+    f32 b;
+};
+
+struct UV final {
+    f32 u;
+    f32 v;
+};
+}  // namespace rin
