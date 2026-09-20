81ae489c71eb2c6f7e6eedac70fd608ac515cc75
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-20 18:37:06 +0000

    begin_frameのシグネチャを変更

--- include/window.hpp
+++ include/window.hpp
@@ -46,7 +46,7 @@ class Window final {
     ) noexcept -> std::expected<Window, Error> {
         // GLFWの初期化（何回呼び出しても安全）
         if (not static_cast<bool>(glfwInit()))
-            return Error::create(Error::Type::runtime, "Failed to GLFW initialize");
+            return Error::create(Error::runtime, "Failed to GLFW initialize");
 
         // これから作る画面のメタ設定
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -57,8 +57,7 @@ class Window final {
         auto        str    = std::string{title};
         auto* const window = glfwCreateWindow(width, height, str.c_str(), nullptr, nullptr);
 
-        if (window == nullptr)
-            return Error::create(Error::Type::runtime, "Failed to initialize window");
+        if (window == nullptr) return Error::create(Error::runtime, "Failed to initialize window");
 
         glfwMakeContextCurrent(window);
         gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));  // NOLINT
@@ -69,7 +68,7 @@ class Window final {
         auto shader = Shader::create();
 
         if (not shader)
-            return Error::create(Error::Type::runtime, "Failed to create shader", shader.error());
+            return Error::create(Error::runtime, "Failed to create shader", shader.error());
 
         return Window{window, std::move(*shader)};
     }
@@ -98,9 +97,11 @@ class Window final {
         return not static_cast<bool>(glfwWindowShouldClose(window_));
     }
 
-    void begin_frame() noexcept {
+    void begin_frame(
+        const f32 r = 0.f, const f32 g = 0.f, const f32 b = 0.f, const f32 alpha = 1.f
+    ) noexcept {
         glfwPollEvents();
-        glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+        glClearColor(r, g, b, alpha);
         glClear(GL_COLOR_BUFFER_BIT);
         shader_.use();
     }
