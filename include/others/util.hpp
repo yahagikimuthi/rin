9d230e72c8bb094fb0549ddad17ad5d21426e36b
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-22 05:47:33 +0000

    命名規則を標準ライブラリと同様のものに変更

--- include/others/util.hpp
+++ include/others/util.hpp
@@ -6,12 +6,15 @@
 
 namespace rin {
 template <typename T, typename Variant>
-struct is_variant_member_impl : std::false_type {};
+struct is_variant_member : std::false_type {};
 
 template <typename T, typename... Ts>
-struct is_variant_member_impl<T, std::variant<Ts...>>
+struct is_variant_member<T, std::variant<Ts...>>
     : std::bool_constant<(std::is_same_v<T, Ts> or ...)> {};
 
 template <typename T, typename Variant>
-concept variant_member = is_variant_member_impl<T, Variant>::value;
+concept variant_member = is_variant_member<T, Variant>::value;
+
+template <typename T, typename Variant>
+static constexpr auto is_variant_member_v = is_variant_member<T, Variant>::value;
 }  // namespace rin
