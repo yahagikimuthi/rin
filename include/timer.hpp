8c0c65a5ebbf2d3b361b3fad69ab4c36dd28b2f3
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-23 02:24:00 +0000

    timer::tickはdeltatimeを戻り値とするよう変更

--- include/timer.hpp
+++ include/timer.hpp
@@ -8,10 +8,11 @@
 namespace rin {
 class timer final {
   public:
-    static void tick() noexcept {
+    static auto tick() noexcept -> f32 {
         const auto current_time = glfwGetTime();
         delta_time_             = current_time - last_time_;
         last_time_              = current_time;
+        return static_cast<f32>(delta_time_);
     }
 
     [[nodiscard]] static constexpr auto delta_time() noexcept -> f32 {
