6b1b98459d50870ae168a41fb45cb78b8c2ccf2f
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-23 18:30:55 +0000

    shaderにわたすときに255で色を割らなければならない。

--- include/renderer/renderer.hpp
+++ include/renderer/renderer.hpp
@@ -65,7 +65,7 @@ class renderer final {
 
     void draw(const vertex_vector& vec, const camera& camera) noexcept {
         shader_.set_mat4(shader::u_Transform, camera.calc_view_position_mat());
-        shader_.set_vec4(shader::u_Color, static_cast<glm::vec4>(white));
+        shader_.set_vec4(shader::u_Color, static_cast<glm::vec4>(white) / 255.f);
         shader_.set_bool(shader::u_UseTexture, false);
 
         mesh_.update_vertices(vec);
