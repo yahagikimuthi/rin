9bff77f5cc54e3c784e7bc3f3b7c8f168f0a505d
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-24 10:40:08 +0000

    textureのゲッターを追加

--- include/texture/font.hpp
+++ include/texture/font.hpp
@@ -109,6 +109,8 @@ class font final {
         return std::nullopt;
     }
 
+    [[nodiscard]] auto settle_texture() const noexcept -> const texture& { return atlas_texture_; }
+
     [[nodiscard]] auto font_size() const noexcept -> f32 { return font_size_; }
 
   private:
