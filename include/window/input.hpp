d1e0c3d9d809f32fb6de2380f130a6c63b39bf29
yahagikimuthi <yahagikimuth@gmail.com>
2026-09-26 01:59:02 +0000

    キーのインプットを管理するクラスを分離

--- include/window/input.hpp
+++ include/window/input.hpp
@@ -13,39 +13,75 @@
 #include "window/key.hpp"
 
 namespace rin {
+class key_input final {
+  public:
+    [[nodiscard]] static auto make() noexcept -> key_input { return key_input{}; }
+
+    void update(GLFWwindow* win) noexcept {
+        previous_ = current_;
+
+        for (const auto i : std::views::indices(current_.size())) {
+            current_[i] = (glfwGetKey(win, static_cast<i32>(i))) == GLFW_PRESS;
+        }
+    }
+
+    [[nodiscard]] auto is_key_down(const key key_button) const noexcept -> bool {
+        const auto idx = key_to_size_t(key_button);
+        return current_[idx];
+    }
+
+    [[nodiscard]] auto is_key_pressed(const key key_button) const noexcept -> bool {
+        const auto idx = key_to_size_t(key_button);
+        return current_[idx] and not previous_[idx];
+    }
+
+    [[nodiscard]] auto is_key_released(const key key_button) const noexcept -> bool {
+        const auto idx = key_to_size_t(key_button);
+        return not current_[idx] and previous_[idx];
+    }
+
+  private:
+    explicit key_input() noexcept = default;
+
+    [[nodiscard]] static auto key_to_size_t(const key key_button) noexcept -> std::size_t {
+        return static_cast<std::size_t>(std::to_underlying(key_button));
+    }
+
+    std::array<bool, GLFW_KEY_LAST> current_{};
+    std::array<bool, GLFW_KEY_LAST> previous_{};
+};
+
+[[nodiscard]] inline auto make_key_input() noexcept -> key_input { return key_input::make(); }
+
 class input final {
   public:
     [[nodiscard]] static auto make() noexcept -> input { return input{}; }
 
-    void update(GLFWwindow* window) noexcept {
-        if (window == nullptr) return;
+    void update(GLFWwindow* win) noexcept {
+        if (win == nullptr) return;
 
-        previous_keys_ = current_keys_;
-
-        for (const auto i : std::views::indices(current_keys_.size())) {
-            current_keys_[i] = (glfwGetKey(window, static_cast<int>(i)) == GLFW_PRESS);
-        }
+        key_input_.update(win);
 
         auto x = f64{};
         auto y = f64{};
 
-        glfwGetCursorPos(window, &x, &y);
+        glfwGetCursorPos(win, &x, &y);
         mouse_position_ = {.x = static_cast<f32>(x), .y = static_cast<f32>(y)};
     }
 
-    [[nodiscard]] constexpr auto is_key_down(const key key_button) const noexcept -> bool {
-        const auto idx = key_to_size_t(key_button);
-        return current_keys_[idx];
+    [[nodiscard]] auto is_key_down(const key key_button) const noexcept -> bool {
+        return key_input_.is_key_down(key_button);
     }
-    [[nodiscard]] constexpr auto is_key_pressed(const key key_button) const noexcept -> bool {
-        const auto idx = key_to_size_t(key_button);
-        return current_keys_[idx] and not previous_keys_[idx];
+
+    [[nodiscard]] auto is_key_pressed(const key key_button) const noexcept -> bool {
+        return key_input_.is_key_pressed(key_button);
     }
-    [[nodiscard]] constexpr auto is_key_released(const key key_button) const noexcept -> bool {
-        const auto idx = key_to_size_t(key_button);
-        return not current_keys_[idx] and previous_keys_[idx];
+
+    [[nodiscard]] auto is_key_released(const key key_button) const noexcept -> bool {
+        return key_input_.is_key_released(key_button);
     }
-    [[nodiscard]] constexpr auto mouse_position(const extent& window_size) const noexcept -> vec2 {
+
+    [[nodiscard]] auto mouse_position(const extent& window_size) const noexcept -> vec2 {
         if (window_size.width <= 0.f or window_size.height <= 0.f) return {.x = 0.f, .y = 0.f};
 
         const auto norm_x = mouse_position_.x / window_size.width;
@@ -55,15 +91,13 @@ class input final {
     }
 
   private:
-    explicit constexpr input() noexcept = default;
-    [[nodiscard]] static constexpr auto key_to_size_t(const key key_button) noexcept
-        -> std::size_t {
+    explicit input() noexcept = default;
+    [[nodiscard]] static auto key_to_size_t(const key key_button) noexcept -> std::size_t {
         return static_cast<std::size_t>(std::to_underlying(key_button));
     }
 
-    std::array<bool, GLFW_KEY_LAST + 1> current_keys_{};
-    std::array<bool, GLFW_KEY_LAST + 1> previous_keys_{};
-    vec2                                mouse_position_{};
+    key_input key_input_{make_key_input()};
+    vec2      mouse_position_{};
 };
 
 [[nodiscard]] inline auto make_input() noexcept -> input { return input::make(); }
